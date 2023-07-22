93aec27290966b137bd6e9d05d1300a209a7c8f4
sndvsk <aleksander.daniel.veske@ut.ee>
2023-07-23 00:18:54 +0300

    Code fixup

--- src/main/java/com/example/FoodDeliveryDemoApp/component/userItems/user/controller/AuthenticationController.java
+++ src/main/java/com/example/FoodDeliveryDemoApp/component/userItems/user/controller/AuthenticationController.java
@@ -11,8 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-
 @CrossOrigin
 @RestController
 @RequestMapping("/api/v2/auth")
