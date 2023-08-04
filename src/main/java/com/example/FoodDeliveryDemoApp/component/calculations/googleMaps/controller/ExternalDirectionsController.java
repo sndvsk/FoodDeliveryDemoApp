1d0b2c0a64d8389e2630e3de1ee8c6593eba741d
sndvsk <aleksander.daniel.veske@ut.ee>
2023-08-04 14:17:34 +0300

    upd project

--- src/main/java/com/example/FoodDeliveryDemoApp/component/calculations/googleMaps/controller/ExternalDirectionsController.java
+++ src/main/java/com/example/FoodDeliveryDemoApp/component/calculations/googleMaps/controller/ExternalDirectionsController.java
@@ -1,11 +1,8 @@
 package com.example.FoodDeliveryDemoApp.component.calculations.googleMaps.controller;
 
 import com.example.FoodDeliveryDemoApp.component.address.dto.AddressDTO;
-import com.example.FoodDeliveryDemoApp.component.calculations.deliveryFee.dto.DeliveryFeeDTO;
 import com.example.FoodDeliveryDemoApp.component.calculations.googleMaps.dto.DirectionDTO;
 import com.example.FoodDeliveryDemoApp.component.calculations.googleMaps.service.ExternalDirectionsService;
-import com.example.FoodDeliveryDemoApp.exception.CustomNotFoundException;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
